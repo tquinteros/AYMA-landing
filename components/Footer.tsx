12433b960c97c83ae0a160457a4aefb56a2a6483
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2026-03-19 14:55:44 -0300

    footer mail

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
                     © AYMA Wellness Club l Remeros Beach, Rincon de Milberg l Buenos Aires, Argentina.
                 </p>
                 <div className='flex flex-col sm:flex-row items-center justify-center gap-4 sm:gap-6'>
-                    <a href="mailto:info@aymawellness.com.ar" className='text-primary-500 hover:opacity-75 duration-300 transition-all text-sm sm:text-base break-all text-center'>
+                    <a href="mailto:info@aymawellness.com.ar" className='text-primary-500 uppercase tracking-[6px] hover:opacity-75 duration-300 transition-all text-sm break-all text-center'>
                         info@aymawellness.com.ar
                     </a>
                     <span className='hidden sm:block text-primary-500'>|</span>
@@ -16,7 +16,7 @@ const Footer = () => {
                         <a href="https://wa.me/3333333333" target="_blank" rel="noreferrer">
                             <Image src="/whatsapp.svg" alt="facebook" width={18} height={18} />
                         </a>
-                        <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+                        <a href="https://www.instagram.com/aymawellness/" target="_blank" rel="noreferrer">
                             <Image src="/instagram.svg" alt="instagram" width={18} height={18} />
                         </a>
                     </div>
